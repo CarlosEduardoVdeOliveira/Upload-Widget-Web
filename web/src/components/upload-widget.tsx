f6aede9f1aba2f361fa8abd9a3fe78f6e3004c35
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2026-03-12 11:19:08 -0300

    feat: enhance upload widget with pending upload state and CSS improvements

--- web/src/components/upload-widget.tsx
+++ web/src/components/upload-widget.tsx
@@ -7,29 +7,31 @@ import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
 
 export function UploadSWidget() {
+  const isThereAnyPendingUpload = true;
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
 
   return (
-    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
+    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
       <motion.div
+        data-progress={isThereAnyPendingUpload}
         variants={{
           closed: {
             width: "max-content",
             height: 44,
-            transition:{
-              type: 'inertia',
-            }
+            transition: {
+              type: "inertia",
+            },
           },
           open: {
             width: 360,
             height: "auto",
-            transition:{
-              duration: 0.4
-            }
+            transition: {
+              duration: 0.4,
+            },
           },
         }}
         animate={isWidgetOpen ? "open" : "closed"}
-        className="bg-zinc-900 w-90 overflow-hidden rounded-xl shadow-shape"
+        className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape  data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
       >
         {!isWidgetOpen && <UploadWidgetMinimizedButton />}
         <Collapsible.Content>
